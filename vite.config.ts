67bf0433fe3f47c4dedc62c62310dd470dd18410
nodin <ton.email@domaine>
2026-05-09 10:50:52 +0200

    chore: update vite base path to relative directory for flexible deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/MIN_BOOKMARKS/',
+    base: './',
     plugins: [
       react(),
       tailwindcss(),
